77358155c32ef1a5dfd77ccf12cdb7cf1e47acd9
Ciro S. Costa <cscosta@pivotal.io>
2019-05-21 22:50:58 -0400

    add ability to disable collectors
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- main.go
+++ main.go
@@ -18,8 +18,9 @@ import (
 var (
 	version = "dev"
 	config  = struct {
-		TelemetryPath string `long:"path" default:"/" description:"path to serve metrics"`
-		ListenAddress string `long:"address" default:":9000" description:"address to listen for prometheus scraping"`
+		Disable       []string `long:"disable" short:"d" description:"collectors to disable"`
+		TelemetryPath string   `long:"path" default:"/" description:"path to serve metrics"`
+		ListenAddress string   `long:"address" default:":9000" description:"address to listen for prometheus scraping"`
 
 		Postgres flag.PostgresConfig `group:"PostgreSQL Configuration" namespace:"postgres"`
 	}{}
@@ -50,86 +51,106 @@ func main() {
 		panic(err)
 	}
 
-	exp := exporter.Exporter{
-		TelemetryPath: config.TelemetryPath,
-		ListenAddress: config.ListenAddress,
-		Collectors: []prometheus.Collector{
-			&collectors.Collector{
-				Description: prometheus.NewDesc(
-					"flight_recorder_pipelines",
-					"Number of pipelines set",
-					[]string{"team"},
-					nil,
-				),
-				RetrievalFunc: database.Pipelines,
-			},
-			&collectors.Collector{
-				Description: prometheus.NewDesc(
-					"flight_recorder_containers",
-					"Number of containers",
-					[]string{"type", "worker", "state"},
-					nil,
-				),
-				RetrievalFunc: database.Containers,
-			},
-			&collectors.Collector{
-				Description: prometheus.NewDesc(
-					"flight_recorder_workers",
-					"Per-state worker count",
-					[]string{"state"},
-					nil,
-				),
-				RetrievalFunc: database.Workers,
-			},
-			&collectors.Collector{
-				Description: prometheus.NewDesc(
-					"flight_recorder_resources",
-					"Number of resources configured",
-					[]string{"pipeline", "team"},
-					nil,
-				),
-				RetrievalFunc: database.Resources,
-			},
-			&collectors.Collector{
-				Description: prometheus.NewDesc(
-					"flight_recorder_teams",
-					"Number of teams",
-					nil,
-					nil,
-				),
-				RetrievalFunc: database.Teams,
-			},
-			&collectors.Collector{
-				Description: prometheus.NewDesc(
-					"flight_recorder_builds",
-					"Number of builds",
-					[]string{"status", "team", "pipeline"},
-					nil,
-				),
-				RetrievalFunc: database.Builds,
-			},
-			&collectors.Collector{
-				Description: prometheus.NewDesc(
-					"flight_recorder_pg_table_stat",
-					"PostgreSQL user table statistics",
-					[]string{"statistic", "table"},
-					nil,
-				),
-				RetrievalFunc: database.TableStat,
-			},
-			&collectors.Collector{
-				Description: prometheus.NewDesc(
-					"flight_recorder_pg_table_sizes_bytes",
-					"PostgreSQL table sizes",
-					[]string{"table"},
-					nil,
-				),
-				RetrievalFunc: database.TableSizes,
-			},
+	collectorsMap := map[string]prometheus.Collector{
+		"pipelines": &collectors.Collector{
+			Description: prometheus.NewDesc(
+				"flight_recorder_pipelines",
+				"Number of pipelines set",
+				[]string{"team"},
+				nil,
+			),
+			RetrievalFunc: database.Pipelines,
+		},
+		"containers": &collectors.Collector{
+			Description: prometheus.NewDesc(
+				"flight_recorder_containers",
+				"Number of containers",
+				[]string{"type", "worker", "state"},
+				nil,
+			),
+			RetrievalFunc: database.Containers,
+		},
+		"workers": &collectors.Collector{
+			Description: prometheus.NewDesc(
+				"flight_recorder_workers",
+				"Per-state worker count",
+				[]string{"state"},
+				nil,
+			),
+			RetrievalFunc: database.Workers,
+		},
+		"resources": &collectors.Collector{
+			Description: prometheus.NewDesc(
+				"flight_recorder_resources",
+				"Number of resources configured",
+				[]string{"pipeline", "team"},
+				nil,
+			),
+			RetrievalFunc: database.Resources,
+		},
+		"teams": &collectors.Collector{
+			Description: prometheus.NewDesc(
+				"flight_recorder_teams",
+				"Number of teams",
+				nil,
+				nil,
+			),
+			RetrievalFunc: database.Teams,
+		},
+		"builds": &collectors.Collector{
+			Description: prometheus.NewDesc(
+				"flight_recorder_builds",
+				"Number of builds",
+				[]string{"status", "team", "pipeline"},
+				nil,
+			),
+			RetrievalFunc: database.Builds,
+		},
+		"table-stat": &collectors.Collector{
+			Description: prometheus.NewDesc(
+				"flight_recorder_pg_table_stat",
+				"PostgreSQL user table statistics",
+				[]string{"statistic", "table"},
+				nil,
+			),
+			RetrievalFunc: database.TableStat,
+		},
+		"tables-sizes": &collectors.Collector{
+			Description: prometheus.NewDesc(
+				"flight_recorder_pg_table_sizes_bytes",
+				"PostgreSQL table sizes",
+				[]string{"table"},
+				nil,
+			),
+			RetrievalFunc: database.TableSizes,
 		},
 	}
 
+	for _, collectorToDisable := range config.Disable {
+		_, ok := collectorsMap[collectorToDisable]
+		if !ok {
+			panic(fmt.Errorf("unknown collector to be disabled: " + collectorToDisable))
+		}
+
+		fmt.Println("disabled:\t" + collectorToDisable)
+		delete(collectorsMap, collectorToDisable)
+	}
+
+	var collectors = []prometheus.Collector{}
+	for name, collector := range collectorsMap {
+		fmt.Println("enabled:\t" + name)
+		collectors = append(collectors, collector)
+	}
+
+	exp := exporter.Exporter{
+		TelemetryPath: config.TelemetryPath,
+		ListenAddress: config.ListenAddress,
+		Collectors:    collectors,
+	}
+
 	go handleSignals(&exp, database)
+
+	fmt.Println("listening on ", config.ListenAddress, config.TelemetryPath)
 
 	err = exp.Listen()
 	if err != nil {
